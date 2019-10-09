490b3bf72ef71f1bfa9550b5d8a64de62c5a71d2
pomb95 <paul.buguet@gmail.com>
2019-10-09 15:05:42 +0200

    6 files added

---
+++ src/app/detail/detail.component.ts
@@ -0,0 +1,10 @@
+import { Component, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: [ './detail.component.css' ]
+})
+export class DetailComponent  {
+
+}
